f360162cdd9a027d4537390ddc46b8208b03ba7e
ysicing <root@ysicing.net>
2018-07-16 18:08:27 +0800

    update

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+@app.route('/env')
+def env():
+    html = "System Time: \n\n"
+    html += datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return Response(html, mimetype='text/plain')
